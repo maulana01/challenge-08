16e2338b8e10ceb7fd437294c367ed8fbae37661
Maulana Imam Malik <maulanaimammalik4@gmail.com>
2022-06-09 22:35:00 +0700

    small fix

--- controllers/auth_api.js
+++ controllers/auth_api.js
@@ -3,6 +3,8 @@
 const { UserGame } = require('../models');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const nodemailer = require('nodemailer');
+const MailChecker = require('mailchecker');
 require('dotenv').config();
 
 module.exports = {
@@ -16,22 +18,50 @@ module.exports = {
       const encryptedPassword = await bcrypt.hash(password, 10);
 
       // Create user in our database
-      const user = await UserGame.create({
-        username,
-        email: email.toLowerCase(), // sanitize: convert email to lowercase
-        password: encryptedPassword,
-        id_role: req.body.id_role ? req.body.id_role : 2,
-      });
 
-      // Create token
-      const token = jwt.sign({ id_user: user.id_user }, process.env.TOKEN_KEY, {
-        expiresIn: '15m',
-      });
-      // save user token
-      user.token = token;
+      if (!MailChecker.isValid(req.body.email)) {
+        res.status(401).json({ error: 'Email Tidak Valid' });
+      } else {
+        const user = await UserGame.create({
+          username,
+          email: email.toLowerCase(), // sanitize: convert email to lowercase
+          password: encryptedPassword,
+          id_role: req.body.id_role ? req.body.id_role : 2,
+        });
 
-      // return new user
-      res.status(200).json({ message: 'Berhasil Membuat User Game', result: user });
+        // Create token
+        const token = jwt.sign({ id_user: user.id_user }, process.env.TOKEN_KEY, {
+          expiresIn: '15m',
+        });
+        // save user token
+        user.token = token;
+
+        const transporter = nodemailer.createTransport({
+          service: 'gmail',
+          auth: {
+            user: 'maulanaimammalik4@gmail.com',
+            pass: 'eljjzghkqmaveulm',
+          },
+        });
+
+        const mailOptions = {
+          from: 'maulanaimammalik4@gmail.com',
+          to: req.body.email,
+          subject: 'Welcome Message!',
+          html: '<html><h1>Selamat Datang, Terimakasih sudah mendaftar &#9786;</h1></html>',
+        };
+
+        transporter.sendMail(mailOptions, function (error, info) {
+          if (error) {
+            console.log(error);
+          } else {
+            console.log('Email Sent:', info.response);
+          }
+        });
+
+        // return new user
+        res.status(200).json({ message: 'Berhasil Membuat User Game', result: user });
+      }
     } catch (err) {
       res.status(500).json({ message: 'Gagal Create User Game', err: err.message });
     }
